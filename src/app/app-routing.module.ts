d7e594327050a7ee64686d4f7a1bc20b3943799a
teledi_makhafola <you@example.com>
2023-05-01 20:08:15 +0200

    updated routing for view component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AddemployeeComponent } from './addemployee/addemployee.component';
 import {EditemployeeComponent} from './editemployee/editemployee.component';
+import { ViewemployeeComponent } from './viewemployee/viewemployee.component';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [{path:'home', component: HomeComponent},
 {path:'add-employee',component:AddemployeeComponent},
+{path:'view-employee',component:ViewemployeeComponent},
 {path:'edit-employee',component:EditemployeeComponent},
 {
   path: 'home',
